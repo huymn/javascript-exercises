0ee532728c82bcbae2fa7af5f1838307b27abde4
Huy Nguyen <huynguyen560@gmail.com>
2024-08-11 15:58:30 -0700

    feat(javascript-exercises): complete exercise 04
    Authorized-by: Huy Nguyen (huynguyen560@gmail.com)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...numbers) {
+  for (let i = 0; i < numbers.length; i++) {
+    arr = arr.filter((num) => num !== numbers[i]);
+  }
+
+  return arr;
 };
 
 // Do not edit below this line
