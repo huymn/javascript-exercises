f27af9c6980fa6937e120ada6e3b6baebe48098b
Huy Nguyen <huynguyen560@gmail.com>
2024-08-16 22:52:47 -0700

    feat(javascript-exercises): complete exercise 12
    Authorized-by: Huy Nguyen (huynguyen560@gmail.com)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,33 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce(
+    (oldestPerson, currentPerson) => {
+      if (
+        currentPerson.yearOfBirth !== undefined &&
+        currentPerson.yearOfDeath !== undefined
+      ) {
+        if (
+          currentPerson.yearOfDeath - currentPerson.yearOfBirth >
+          oldestPerson.yearOfDeath - oldestPerson.yearOfBirth
+        ) {
+          oldestPerson = currentPerson;
+        }
+      } else if (
+        currentPerson.yearOfDeath === undefined &&
+        currentPerson.yearOfBirth !== undefined
+      ) {
+        const currentYear = new Date().getFullYear();
+        if (
+          currentYear - currentPerson.yearOfBirth >
+          oldestPerson.yearOfDeath - oldestPerson.yearOfBirth
+        ) {
+          oldestPerson = { ...currentPerson, yearOfDeath: currentYear };
+        }
+      }
 
+      return oldestPerson;
+    },
+    { yearOfBirth: 0, yearOfDeath: 0 }
+  );
 };
 
 // Do not edit below this line
