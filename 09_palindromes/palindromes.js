d59d76698f1c6a976d3530300a3f71a343d722b0
Huy Nguyen <huynguyen560@gmail.com>
2024-08-16 22:13:00 -0700

    feat(javascript-exercises): complete exercise 09
    Authorized-by: Huy Nguyen (huynguyen560@gmail.com)

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let left = 0;
+  let right = str.length - 1;
 
+  while (left <= right) {
+    if (!str.charAt(left).match(/[a-zA-Z0-9]/)) {
+      left++;
+    } else if (!str.charAt(right).match(/[a-zA-Z0-9]/)) {
+      right--;
+    } else if (
+      str.charAt(left).toLowerCase() !== str.charAt(right).toLowerCase()
+    ) {
+      return false;
+    } else {
+      left++;
+      right--;
+    }
+  }
+
+  return true;
 };
 
 // Do not edit below this line
