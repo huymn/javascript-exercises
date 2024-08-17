1a4b440af9190c1dc0cb877e654f168600d9d4e7
Huy Nguyen <huynguyen560@gmail.com>
2024-08-16 22:22:42 -0700

    feat(javascript-exercises): complete exercise 10
    Authorized-by: Huy Nguyen (huynguyen560@gmail.com)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  num = Number.parseInt(num);
+  if (num < 0) return "OOPS";
 
+  if (num === 0) return 0;
+
+  if (num === 1 || num === 2) return 1;
+
+  let prevPrevValue = 1;
+  let prevValue = 1;
+
+  let fibValue = prevPrevValue + prevValue;
+
+  let counter = 3;
+
+  while (counter < num) {
+    prevPrevValue = prevValue;
+    prevValue = fibValue;
+    fibValue = prevPrevValue + prevValue;
+
+    counter++;
+  }
+
+  return fibValue;
 };
 
 // Do not edit below this line
