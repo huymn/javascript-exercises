b7e347991cee98bd785cb4fb09fb5de0f57c6d9b
Huy Nguyen <huynguyen560@gmail.com>
2024-08-16 21:56:21 -0700

    feat(javascript-exercises): complete exercise 08
    Authorized-by: Huy Nguyen (huynguyen560@gmail.com)

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (list) {
+  return list.reduce((sum, currentNumber) => (sum += currentNumber), 0);
 };
 
-const multiply = function() {
-
+const multiply = function (list) {
+  return list.reduce((total, currentNumber) => (total *= currentNumber), 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result = 1;
+
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +35,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
